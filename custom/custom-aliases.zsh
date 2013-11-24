165e50e060667e7243f2a70dca27af17e895b0d2
ta9o <im.ta9o@gmail.com>
2013-11-24 17:19:19 +0900

    first customized aliases

---
+++ custom/custom-aliases.zsh
@@ -0,0 +1,17 @@
+alias ll='ls -la'
+alias lll='ls -la | less'
+
+# git
+alias gad='git add'
+alias gst='git status'
+alias gco='git commit'
+alias gfe='git fetch'
+alias gpu='git push'
+alias glo='git log'
+alias gbr='git branch'
+alias gch='git checkout'
+alias gdi='git diff --color'
+alias gdis='git diff --color --staged'
+
+# for tmux
+alias tmux='tmux -u'
